508460844e267ce4cef30d0c32ce2f175d9b60d5
David Oncu <freelanceroncu@gmail.com>
2022-06-14 12:51:51 +0300

    .

--- function.js
+++ function.js
@@ -1,12 +1,6 @@
 const shareOption = document.querySelector('.card-footer');
 const footerActive = document.querySelector('.card-footer-active')
 
-// document.querySelector('.share-button').addEventListener('click',
-// function () {
-//     this.classList.toggle('active');
-//     shareOption.classList.toggle('active');
-
-// } );
 
 document.querySelector('.share-button').addEventListener('click',
 function () {
